2eac530a0d4439deafbbab815509d182ba7b706e
Taffman91 <alfredskedeback@gmail.com>
2017-02-10 14:44:20 +0100

    DONE!

--- email-kamals-meny/src/mainKamalsMeny.java
+++ email-kamals-meny/src/mainKamalsMeny.java
@@ -11,29 +11,8 @@ public class mainKamalsMeny {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		Document d=Jsoup.connect("https://www.facebook.com/Kamalsrestaurang/").timeout(6000).get();
-		String str = Jsoup.connect("https://www.facebook.com/Kamalsrestaurang/").get().text();
-		
-		System.out.println(str);
-		
-		String title = d.title();
-		System.out.println("Title:"+title);
-		
-		Elements links = d.select("div[class]");
-		Elements meny =d.select("Meny vecka 6");
-		
-		int antal = 0;
-			
-		die:
-		
-		for (Element link: links)
-		{
-			System.out.println("\nLink: "+link.attr("<p>"));
-			System.out.println("text: "+link.text());
-			
-			antal++;
-			if (antal > 70)
-				break die;
+		Document doc=Jsoup.connect("https://www.facebook.com/Kamalsrestaurang/").timeout(6000).get();
+		String Text = doc.select("body p").text();
+		System.out.println(Text);
 		}
 	}
-}
